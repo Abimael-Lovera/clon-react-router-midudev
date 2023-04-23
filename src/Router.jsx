e83a803d35f674ad552a8264dc64b2cb35ad025b
Abimael Lovera <abimaellovera@gmail.com>
2023-04-23 18:51:16 -0300

    Componente < Route /> para crear rutas declarativas

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Children } from 'react'
 import { EVENTS } from './conts'
 import { match } from 'path-to-regexp'
 
-export function Router ({ routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
+export function Router ({ children, routes = [], defaultComponent: DefaultComponent = () => <h1>404</h1> }) {
   const [currentPath, setCurrentPath] = useState(window.location.pathname)
 
   useEffect(() => {
@@ -21,7 +21,22 @@ export function Router ({ routes = [], defaultComponent: DefaultComponent = () =
 
   let routeParams = {}
 
-  const Page = routes.find(({ path }) => {
+  // add routes del children <Route /> component
+  // leer mas props del children
+  const routesFromChildren = Children.map(children, ({ props, type }) => {
+    const { name } = type
+    const isRoute = name === 'Route'
+
+    if (!isRoute) return null
+
+    return props
+  })
+
+  // concatenando las rutas que recibe Router con el children
+  const routesToUse = routesFromChildren.concat(routes)
+  // console.log(routesToUse)
+
+  const Page = routesToUse.find(({ path }) => {
     if (path === currentPath) return true
 
     // hemos usado path-to-regexp
@@ -40,5 +55,6 @@ export function Router ({ routes = [], defaultComponent: DefaultComponent = () =
     return true
   }
   )?.Component
+
   return Page ? <Page routeParams={routeParams} /> : <DefaultComponent routeParams={routeParams} />
 }
