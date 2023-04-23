3b7bfd9b10692fcae4163480bd9190e521f8d12e
Abimael Lovera <abimaellovera@gmail.com>
2023-04-22 21:28:51 -0300

    Criando rutas SPAs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate (href) {
+  window.history.pushState({}, '', href)
+  // crear un evento personalizado
+  const navigationEvent = new Event(NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent)
+}
 function HomePage () {
   return (
     <>
       <h1>Home</h1>
       <p>Este es una página de ejemplo para crea un React Router desde cero</p>
-      <a href='/about'>Ir a Sobre nosotros</a>
+      <button onClick={() => { navigate('/about') }}>Ir a Sobre nosotros</button>
     </>
   )
 }
@@ -17,13 +25,25 @@ function AboutPage () {
       <h1>About</h1>
       <img src='https://randomuser.me/api/portraits/men/11.jpg' alt='' />
       <p>Hola! Me llamo Abimael Lovera y estoy creando un clon de React Router.</p>
-      <a href='/'>Ira a la Home </a>
+      <button onClick={() => { navigate('/') }}>Ira a la Home </button>
     </>
   )
 }
 
 function App () {
   const [current, setCurrent] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrent(window.location.pathname)
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+    }
+  }, [])
 
   return (
     <main>
