bd0ba05a12f84e695aacd04570ce58cac944dc81
Abimael Lovera <abimaellovera@gmail.com>
2023-04-22 21:06:14 -0300

    MPAs

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Crea un React Router desde cero
 
+- [x] Instalar el linter
+- [x] Crear una forma de hacer MPAs (Multiple Page Application)
 - [] Crea una forma de hacer SPAs
 - [] Poder navegar entre páginas con el botón de atrás
-- [] Crear componente Link para hacerlo declarativo 
+- [] Crear componente Link para hacerlo declarativo
 - [] Crear componente Router para hacerlo mas declarativo
 - [] Soportar ruta por defecto (404)
 - [] Soportar rutas con parámetros
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function HomePage () {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Home</h1>
+      <p>Este es una página de ejemplo para crea un React Router desde cero</p>
+      <a href='/about'>Ir a Sobre nosotros</a>
     </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>About</h1>
+      <img src='https://randomuser.me/api/portraits/men/11.jpg' alt='' />
+      <p>Hola! Me llamo Abimael Lovera y estoy creando un clon de React Router.</p>
+      <a href='/'>Ira a la Home </a>
+    </>
+  )
+}
+
+function App () {
+  const [current, setCurrent] = useState(window.location.pathname)
+
+  return (
+    <main>
+      {current === '/' && <HomePage />}
+      {current === '/about' && <AboutPage />}
+    </main>
   )
 }
 
